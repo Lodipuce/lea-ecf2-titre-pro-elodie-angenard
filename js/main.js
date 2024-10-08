41c0b45db1ffb24744a0dbd869a7510d2c22a88e
Elodie Angenard <ange.elodie@gmail.com>
2024-10-08 13:15:44 +0200

    correction erreur contrainte formulaire

--- js/main.js
+++ js/main.js
@@ -68,24 +68,21 @@ form.addEventListener('submit', (e) => {
         // empecher la soumission du formulaire
         e.preventDefault();
 
-        // créer le message d'avertissement dans une div#warning
+        // creer le message d'avertissement dans une div#warning
         const warning = document.createElement('div');
         warning.setAttribute('id', 'warning');
         warning.innerHTML = "Le message doit contenir au moins 15 caractères.";
 
-        // insérer la div#warning à la fin du form
+        // inserer la div#warning à la fin du form
         form.appendChild(warning);
 
     } else {
-        // créer le message OK dans une div#submitted
+        // creer le message OK dans une div#submitted
         const submitted = document.createElement('div');
         submitted.setAttribute('id', 'submitted');
         submitted.innerHTML = "Message soumis avec succès !";
 
-        // effacer la div#warning
-        warning.remove();
-
-        // insérer la div#submitted à la fin du form
-        form.appendChild(submitted);
+        // si elle existe, effacer la div#warning puis inserer la div#submitted
+        document.getElementById('warning') ? warning.remove(): form.appendChild(submitted);
     }
 })
